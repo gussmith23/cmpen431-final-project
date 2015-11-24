a875f24853a2e055d1df6ef3ef5c6a0698160944
gussmith23 <hfs5022@psu.edu>
2015-11-24 16:50:14 -0500

    Adds dictionary for looking up settings

--- config-generator/config-generator.py
+++ config-generator/config-generator.py
@@ -1,3 +1,7 @@
+# Config Generation Script
+# 
+# Author: Gus Smith <hfs5022@psu.edu>
+#
 # Arguments:
 # argv[1] - the absolute path of the final project directory. this is just for testing purposes at the moment;
 #		i'm trying to avoid the headache of figuring out the intracacies of current working directories
@@ -8,8 +12,64 @@ import subprocess
 import sys
 import os
 
+
+#######################
+## Fields
+
+# These keys are used to lookup a string which is then passed
+#	to sed for find-and-replace.
+# The values should each map to a single line; i.e., if we use this
+#	dictionary to find-and-replace a setting, we won't overwrite
+#	multiple lines.
+simplesim_settings = {
+	ifqsize: 	"-fetch:ifqsize",
+	fspeed: 	"-fetch:speed",
+	fmplat:		"-fetch:mplat",
+	bpred:		"-bpred ",		# This one may be an issue...
+	ras:		"-bpred:ras",
+	dwidth:		"-decode:width",
+	iwidth:		"-issue:width",
+	inorder:	"-issue:inorder",
+	wrongpath:	"-issue:wrongpath",
+	ruusize:	"-ruu:size",
+	lsqsize:	"-lsq:size",
+	ialu:		"-res:ialu",
+	imult:		"-res:imult",
+	fpalu:		"-res:fpalu",
+	fpmult:		"-res:fpmult",
+	memport:	"-res:memport",
+	il1:		"-cache:il1 ",
+	il2:		"-cache:il2 ",
+	dl1:		"-cache:dl1 ",
+	dl2:		"-cache:dl2 ",
+	itlb:		"-tlb:itlb",
+	dtlb:		"-tbl:dtlb",
+	il1lat:		"-cache:il1lat",
+	il2lat:		"-cache:il2lat",
+	dl1lat:		"-cache:dl1lat",
+	dl2lat:		"-cache:dl2lat",
+	memlat:		"-mem:lat",
+	memwidth:	"-mem:width",
+	redir:		"-redir:sim"
+}
+
+
 ########################
 ## FUNCTIONS
+
+
+## BASH SCRIPT WRAPPERS
+# new_working_set, modify_working_set, merge_working_set
+# These functions wrap scripts which modify the filesystem directly.
+# These scripts create "working sets" - groups of configs that are all modified
+# together. practically, this allows us to create a new working set (which will
+# copy all existing configs to a new location), modify the working set (which
+# changes the same setting in all configs), and merge the working set (which dumps
+# the new configs back into the old config directory). The result is that we
+# will have doubled the number of configs, each new config being the same as
+# an old config plus one single settings change (done in the modify working 
+# set step).
+
 
 # new_working_set
 #	Wrapper for new-working-set.sh, which generates a new "working set"; i.e.
